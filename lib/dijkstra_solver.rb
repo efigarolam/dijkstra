6b23c94fb444039396167bae22f3bd6f2e311c7c
Eduardo Figarola <eduardo.figarola@crowdint.com>
2013-11-04 21:37:03 -0600

    - Adds DijkstraSolver class. - Adds specs for DijkstraSolver class.

--- lib/dijkstra_solver.rb
+++ lib/dijkstra_solver.rb
@@ -1,3 +1,68 @@
 class DijkstraSolver
+  attr_accessor :graph, :start_node, :end_node, :unvisited_neighbors, :current_node
+
+  def setup(graph)
+    @graph = graph
+  end
+
+  def set_start_node(node_name)
+    @start_node = graph.select_node node_name
+    prepare_start_node
+  end
+
+  def set_end_node(node_name)
+    @end_node = graph.select_node node_name
+  end
+
+  def dijkstra
+    unvisited_neighbors[:nodes].each_with_index do |node, index|
+      distance = current_node.tentative_distance
+
+      distance += distances[index]
+      node.tentative_distance = distance if distance < node.tentative_distance
+    end
+
+    change_current_node
+    if end_node.visited?
+      end_node.tentative_distance
+    else
+      dijkstra
+    end
+  end
+
+  private
+
+  def prepare_start_node
+    @start_node.tentative_distance = 0
+    @start_node.mark_as_current
+  end
+
+  def unvisited_neighbors
+    neighbors = {nodes: [], distances: []}
+
+    graph.edges.each do |edge|
+      node = edge.select_complement_node(current_node) if edge.nodes.include? current_node
+      neighbors[:nodes] << node if node
+      neighbors[:distances] << edge.value if (node && edge.nodes.include?(current_node))
+    end
+
+    neighbors
+  end
+
+  def distances
+    unvisited_neighbors[:distances]
+  end
+
+  def current_node
+    graph.nodes.select { |node| node.current? }.pop
+  end
+
+  def change_current_node
+    new_current_node = unvisited_neighbors[:nodes].sort_by { |node| node.tentative_distance }.first
+    current_node.set_not_current
+
+    new_current_node.mark_as_current
+  end
+
 
 end
--- spec/lib/dijkstra_solver_spec.rb
+++ spec/lib/dijkstra_solver_spec.rb
@@ -1,9 +1,76 @@
 require "spec_helper"
 
 describe DijkstraSolver do
+  let(:node_1) { Node.new("1") }
+  let(:node_2) { Node.new("2") }
+  let(:node_3) { Node.new("3") }
+  let(:node_4) { Node.new("4") }
+  let(:node_5) { Node.new("5") }
+  let(:node_6) { Node.new("6") }
+
+  let(:nodes) do
+    [
+      node_1,
+      node_2,
+      node_3,
+      node_4,
+      node_5,
+      node_6
+    ]
+  end
+
+  let(:edges) do
+    [
+      Edge.new([node_1, node_2], 7),
+      Edge.new([node_1, node_6], 14),
+      Edge.new([node_1, node_3], 9),
+      Edge.new([node_2, node_3], 10),
+      Edge.new([node_2, node_4], 15),
+      Edge.new([node_3, node_4], 11),
+      Edge.new([node_3, node_6], 2),
+      Edge.new([node_4, node_5], 6),
+      Edge.new([node_6, node_5], 9)
+    ]
+  end
+
+  let(:graph) { Graph.new }
+
   subject { DijkstraSolver.new }
 
-  it "accepts a graph" do
-    subject.class.should == DijkstraSolver
+  before do
+    graph.set_nodes nodes
+    graph.set_edges edges
+    subject.setup graph
+  end
+
+  describe '#setup' do
+    its(:graph) { should == graph }
+  end
+
+  describe '#set_start_node' do
+    before { subject.set_start_node node_1.name }
+
+    it "sets the start_node property" do
+      subject.start_node.name.should == node_1.name
+    end
+  end
+
+  describe '#set_end_node' do
+    before { subject.set_end_node node_5.name }
+
+    it "sets the start_node property" do
+      subject.end_node.name.should == node_5.name
+    end
+  end
+
+  describe '#dijkstra' do
+    before do
+      subject.set_start_node node_1.name
+      subject.set_end_node node_5.name
+    end
+
+    it "returns the shortest distance" do
+      subject.dijkstra.should == 20
+    end
   end
 end
